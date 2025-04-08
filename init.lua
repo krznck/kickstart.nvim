18b1df06c0ca5673acd2c56f322b9c91e376894f
krznck <krznck@proton.me>
2025-04-08 16:55:44 +0200

    build: enable reorder-python-imports formatter

--- init.lua
+++ init.lua
@@ -767,7 +767,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'black' },
+        python = { 'reorder-python-imports', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
