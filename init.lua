2eef9fc0682f7edcdbb2159b93f7451f391785fb
krznck <krznck@proton.me>
2025-10-16 19:59:10 +0200

    add autopairs

--- init.lua
+++ init.lua
@@ -1117,6 +1117,13 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
